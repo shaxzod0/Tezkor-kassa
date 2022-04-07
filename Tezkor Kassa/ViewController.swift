0cbd6b85672b342ef431d2b7698b12068426bb94
Shakhzod <azamatjonov90@gmail.com>
2022-04-07 13:05:47 +0500

    Initial Commit

---
+++ Tezkor Kassa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tezkor Kassa
+//
+//  Created by Shaxzod Azamatjonov on 07/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
